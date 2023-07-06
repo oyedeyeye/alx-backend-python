7437f6850749f471a52dfbe7a31a1c2d4ca944f2
Hezekiah Oyemade <oyedey@gmail.com>
2023-07-06 14:21:47 +0100

    task 1: Basic annotation - Concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""concat: function that takes a string str1 and a string str2 as arguments
+and returns a concatenated string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Returns a concatenated string using the arguments
+        Args:
+            str1 (str): first string argument
+            str2 (str): second string argument
+    """
+    return str1 + str2
