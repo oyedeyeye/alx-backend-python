3bb9705dbc5493d9c515a6088c72072c6a91d76f
Hezekiah Oyemade <oyedey@gmail.com>
2023-07-10 16:09:23 +0100

    task 4: Tasks

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+task_wait_n: async coroutine that alter `wait_n` into a new function.
+The code is nearly identical except task_wait_random is being called
+"""
+
+
+import asyncio
+from typing import List
+task_wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    return the list of all the delays (float values) sorted in
+    ascending order without using sort()
+    """
+    delays = [await task_wait_random(max_delay) for _ in range(n)]
+    sorted_list = []
+    while delays:
+        min_Num = delays[0]
+        for i in delays:
+            if i < min_Num:
+                min_Num = i
+        sorted_list.append(min_Num)
+        delays.remove(min_Num)
+    return sorted_list
