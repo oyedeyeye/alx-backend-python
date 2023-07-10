8dadbd4cc808a9b9327e7d7724ceb4c38eceb4b8
Hezekiah Oyemade <oyedey@gmail.com>
2023-07-10 22:43:08 +0100

    task 4 newest update

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -16,4 +16,12 @@ async def task_wait_n(n: int, max_delay: int) -> List[float]:
     ascending order without using sort()
     """
     delays = [await task_wait_random(max_delay) for _ in range(n)]
-    return sorted(delays)
+    sorted_list = []
+    while delays:
+        min_Num = delays[0]
+        for i in delays:
+            if i < min_Num:
+                min_Num = i
+        sorted_list.append(min_Num)
+        delays.remove(min_Num)
+    return sorted_list
