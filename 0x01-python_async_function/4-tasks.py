92e95bacb3c870abc5595b5a9dabe5873f47edf0
Hezekiah Oyemade <oyedey@gmail.com>
2023-07-10 22:39:25 +0100

    task 4 latest update

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -7,7 +7,7 @@ The code is nearly identical except task_wait_random is being called
 
 import asyncio
 from typing import List
-task_wait_random = __import__('0-basic_async_syntax').wait_random
+task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
