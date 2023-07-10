15334b39f244b4684aa3a14160ade43f68d15df9
Hezekiah Oyemade <oyedey@gmail.com>
2023-07-10 16:48:17 +0100

    task 4 updated

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -8,7 +8,7 @@ The code is nearly identical except task_wait_random is being called
 import asyncio
 from typing import List
 import random
-
+task_wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
@@ -16,11 +16,8 @@ async def task_wait_n(n: int, max_delay: int) -> List[float]:
     return the list of all the delays (float values) sorted in
     ascending order without using sort()
     """
-    task_wait_random = __import__('0-basic_async_syntax').wait_random
-
     delays = [await task_wait_random(max_delay) for _ in range(n)]
     sorted_list = []
-
     while delays:
         min_Num = delays[0]
         for i in delays:
@@ -28,5 +25,4 @@ async def task_wait_n(n: int, max_delay: int) -> List[float]:
                 min_Num = i
         sorted_list.append(min_Num)
         delays.remove(min_Num)
-
     return sorted_list
